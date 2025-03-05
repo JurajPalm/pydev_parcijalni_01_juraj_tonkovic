57da6f5d32615e1b4442c3cd8d28b95b2370d3a3
JurajPalm <tonkovicjuraj@gmail.com>
2025-03-05 23:13:06 +0100

    Update

--- products.py
+++ products.py
@@ -8,6 +8,9 @@ class Product:
         self.name = name
         self.price = price
         self.description = description
-              
+    #nakon unosa argumenata za funkciju def__init__, Python prvo kreira varijablu self u koju sprema unesene podatke o objektu
+    #Python zatim zapisuje kreirani objekt u varijablu self na način koji definira funkcija def__repr__(self)
+    #Python zatim dodijeljuje varijabli self zeljeni naziv objekta (npr. laptop)
+    #svaki objekt se pohranjuje na svoju lokaciju u memoriji
     def __repr__(self) -> str: #repr je posebna metoda (funkcija unutar klase) koja reprezentira objekt klase kao string
         return f'repr Naziv: {self.name}'
